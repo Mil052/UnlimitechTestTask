3362acdb3f0d30d284283c45bf982f7f9e30ece2
Miłosz Gajda <gajda.milosz@gmail.com>
2025-09-01 12:10:39 +0200

    feat: create main page content, with basic positioning, setting up Bootstrap navbar with dropdown, setting up Slick Carousel in intro section, and promoted section, setting up Bootstrap accordion in footer navigation

--- src/main.js
+++ src/main.js
@@ -7,7 +7,36 @@ $( () => {
   // Jquery Test
   $('#app').text('jQuery is working');
 
+  // Initializing Intro Slick Carousel
+  $('.intro-carousel').slick({
+    autoplay: true,
+    infinite: true,
+    dots: true,
+  });
+
   // Initializing Slick Slider
-  $('.your-class').slick();
+  $('.promoted-carousel').slick({
+    autoplay: true,
+    infinite: true,
+    dots: true,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        }
+      },
+      {
+        breakpoint: 640,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      }
+    ]
+  });
 
 });
